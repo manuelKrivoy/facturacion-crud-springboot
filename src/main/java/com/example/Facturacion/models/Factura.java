662278bdf1fbeaed62cc7c3a396465dc462d824a
Manuel Krivoy <krivoymanuel@gmail.com>
2024-02-07 17:24:20 -0300

    Consumo API externa para generar la fecha y hora de creación de la factura

--- src/main/java/com/example/Facturacion/models/Factura.java
+++ src/main/java/com/example/Facturacion/models/Factura.java
@@ -2,8 +2,10 @@ package com.example.Facturacion.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 //Defino la entidad Factura, con sus atributos
 @Entity //configuramos para que sea una Entity
@@ -14,7 +16,7 @@ public class Factura {
     private Long id;
 
     @Column(name = "created_at")
-    private LocalDate createdAt; //utilizo LocalDate para que almacene la fecha en la que la factura es creada
+    private String createdAt; //utilizo LocalDate para que almacene la fecha en la que la factura es creada
 
     private double total;
 
@@ -24,7 +26,17 @@ public class Factura {
 
     @PrePersist
     protected void onCreate() {
-        createdAt = LocalDate.now();
+        // Realizamos una solicitud GET a la API externa timeapi.io
+        RestTemplate restTemplate = new RestTemplate();
+        final String apiUrl = "https://timeapi.io/api/Time/current/zone?timeZone=America/Argentina/Buenos_Aires"; // Uso URL para la hora de Buenos Aires
+        TimeApi tiempo = restTemplate.getForObject(apiUrl,TimeApi.class);
+
+        // Almacenamos la hora obtenida en createdAt
+        if (tiempo != null && tiempo.getDateTime() != null) {
+            createdAt = tiempo.getDateTime();
+        } else {
+            createdAt = LocalTime.now().toString(); //Si no hay respuesta o es mala almacenamos la hora de otra forma. (LocalDate)
+        }
     }
 }
 
